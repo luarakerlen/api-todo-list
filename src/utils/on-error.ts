2f1e1c7c8d33d5c7208f3498f548aae0958087a2
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2026-04-21 15:13:33 -0300

    feat: Handles Prisma duplicate entry errors gracefully
    Improves error handling by adding a specific response for known
    Prisma duplicate entry errors, returning a 409 status with a
    clear validation message. Enhances API reliability and provides
    more informative feedback to clients on unique constraint violations.

--- src/utils/on-error.ts
+++ src/utils/on-error.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 import { HTTPError } from "./http.error";
 import { HTTPResponse } from "./http.response";
@@ -10,6 +11,25 @@ export function onError(error: unknown, res: Response): Response {
       statusCode: error.statusCode,
       message: error.message,
       details: error.details,
+    });
+  }
+
+  if (error instanceof PrismaClientKnownRequestError) {
+    let statusCode = 500
+    let message = "Internal server error"
+    let details = error.message
+
+    if (error.code === 'P2002') {
+      message = "Falha de validação: valor duplicado"
+      details = `${error.meta?.target} já existe.`;
+      statusCode = 409;
+    }
+
+    return HTTPResponse({
+      res,
+      statusCode,
+      message,
+      details,
     });
   }
 
