e59fcf0c92388fa7dc970f001c3a7355b129a2a7
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2026-04-24 15:42:36 -0300

    feat: Atualiza testes do método listTasks no TaskService para incluir retorno de tarefas mockadas

--- tests/services/task.service.spec.ts
+++ tests/services/task.service.spec.ts
@@ -1,10 +1,8 @@
 import { TaskService } from "../../src/services/task.service";
-import { prismaMock } from "../config/prisma.mock";
 
-prismaMock.task.findMany.mockResolvedValue([
+const tasks = [
   {
     id: "task1",
-    userId: "12345",
     title: "Task 1",
     description: "Descrição da Task 1",
     status: "pending",
@@ -13,14 +11,13 @@ prismaMock.task.findMany.mockResolvedValue([
   },
   {
     id: "task2",
-    userId: "12345",
     title: "Task 2",
     description: "Descrição da Task 2",
     status: "completed",
     createdAt: new Date(),
     updatedAt: new Date(),
   }
-])
+]
 
 describe("Testes para o método create de task.service", () => {
   test("deveria retornar status 201 ao criar uma nova task", async () => {
@@ -31,12 +28,18 @@ describe("Testes para o método create de task.service", () => {
     const sut = new TaskService(mockDependency);
 
     // Mock para taskRepository.listTasks deve retornar uma lista de tasks para o usuário especificado
+    mockDependency.listTasks = jest.fn().mockResolvedValue(tasks.map((task) => {
+      return {
+        ...task,
+        userId,
+      }
+    }));
 
     // 2 - Act
     const result = await sut.listTasks({ userId });
 
     // 3 - Assert
     expect(result).toBeDefined();
-    expect(result).toHaveProperty("data");
+    expect(result).toHaveProperty("data", tasks);
   })
 })
